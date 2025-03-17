a76d114ff9978c4a65e8b55c9a5642ce62eabba4
lvcky-gg <johnodonnell1997@icloud.com>
2025-03-17 08:44:52 -0400

    editted

--- hypr/hyprland.nix
+++ hypr/hyprland.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}:
 {
       programs.kitty.enable = true;
-      wayland.windowManager.hyprland.enable = true;
+      wayland.windowManager.hyprland={
+
+ 	enable = true;
+	package = null;
+	portalPackage = null;
+};
 }
