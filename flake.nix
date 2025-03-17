0bbf3b5c1b71e79dc37bbc1f3664632972bb0e3a
lvcky-gg <johnodonnell1997@icloud.com>
2025-03-17 12:07:40 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."lvcky" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."lvcky" = self.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
