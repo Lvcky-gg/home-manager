704e8a52e08dc650b8542ec8b95fda8ffd111b6a
lvcky-gg <johnodonnell1997@icloud.com>
2025-03-17 14:24:01 -0400

    chore: codium init

--- browser/fox.nix
+++ browser/fox.nix
@@ -2,5 +2,25 @@
 {
     programs.firefox = {
         enable = true;
+        languagePacks = [ "de" "en-US" ];
+         policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value= true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified";
     };
 }
